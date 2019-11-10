a407fc1d377ef12c84b77050f7c3b4ab07a80615
okram ovic <okram@protonmail.ch>
2019-11-10 11:24:01 +0100

    init repo with version zero

---
+++ precache-manifest.196b49671bc6e5abe1ecb98086534561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1a20ba9909e00bfde0c16877a9e4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d309518a37685f8a1f6c",
+    "url": "/static/css/main.262945e4.chunk.css"
+  },
+  {
+    "revision": "cf672c1ddcd4f90b6515",
+    "url": "/static/js/2.a614d23a.chunk.js"
+  },
+  {
+    "revision": "d309518a37685f8a1f6c",
+    "url": "/static/js/main.fa629802.chunk.js"
+  },
+  {
+    "revision": "803a6744e84c355702d1",
+    "url": "/static/js/runtime-main.22c19486.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
